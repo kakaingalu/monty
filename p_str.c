d59a617028b57bbc0f3623c8c0af882b5c73af37
Khaikhai01 <shishchei@gmail.com>
2022-12-15 23:22:28 +0300

    task completed

--- p_str.c
+++ p_str.c
@@ -4,7 +4,7 @@
 * f_pstr - function that prints the string starting at the top of the stack,
 * followed by a new
 * @head: head of stack
-* @counter: line count
+* @counter: line counter
 * Return: nothing
 */
 void f_pstr(stack_t **head, unsigned int counter)
